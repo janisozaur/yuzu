e1d74aa1f5efcf23d9eab7054a7b2840bec07d5d
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-06-04 16:31:23 +0200

    Update build.sh

--- .travis/macos/build.sh
+++ .travis/macos/build.sh
@@ -6,9 +6,14 @@ export MACOSX_DEPLOYMENT_TARGET=10.12
 export Qt5_DIR=$(brew --prefix)/opt/qt5
 export UNICORNDIR=$(pwd)/externals/unicorn
 
+ls /opt/libexec
 mkdir build && cd build
+export PATH=/opt/libexec:$PATH
+ccache --show-stats > ccache_before
 cmake --version
 cmake .. -DYUZU_BUILD_UNICORN=ON -DCMAKE_BUILD_TYPE=Release
 make -j4
+ccache --show-stats > ccache_after
+diff -U100 ccache_before ccache_after || true
 
 ctest -VV -C Release
