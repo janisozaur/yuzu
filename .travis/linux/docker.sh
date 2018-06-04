42d0f4202f46acc9a0c99b5f6ad9e1b1ea8dfc3b
Michał Janiszewski <janisozaur@users.noreply.github.com>
2018-06-04 15:02:11 +0200

    Install ccache inside docker, report stats

--- .travis/linux/docker.sh
+++ .travis/linux/docker.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -ex
 
 apt-get update
-apt-get install -y build-essential git libqt5opengl5-dev libsdl2-dev libssl-dev python qtbase5-dev wget
-
-# Get a recent version of CMake
-wget https://cmake.org/files/v3.10/cmake-3.10.1-Linux-x86_64.sh
-sh cmake-3.10.1-Linux-x86_64.sh --exclude-subdir --prefix=/ --skip-license
+apt-get install -y build-essential git libqt5opengl5-dev libsdl2-dev libssl-dev python qtbase5-dev wget ccache
 
 cd /yuzu
 
 mkdir build && cd build
+ccache --show-stats > ccache_before
 cmake .. -DYUZU_BUILD_UNICORN=ON -DCMAKE_BUILD_TYPE=Release
 make -j4
+ccache --show-stats > ccache_after
+diff -U100 ccache_before ccache_after || true
 
 ctest -VV -C Release
